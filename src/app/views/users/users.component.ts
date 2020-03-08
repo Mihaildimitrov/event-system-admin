deb71fec08a0318566545650ba8c44e5dc7d6176
mihaildimitrov <mihail.ko.dimitrov@gmail.com>
2020-03-08 20:24:55 +0200

    users list

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -11,46 +11,72 @@ export class UsersComponent implements OnInit {
   constructor(private dataService: DataService) { }
 
   stillLoading: boolean = true;
+  stillLoadingUsersPage: boolean = false;
   searchWord: string = '';
   users = [];
-  pagesStartPoints = [];
-  private usersDocs = [];
+  pagesFirstDocuments = [];
+  pagesLastDocuments = [];
+  showPrevBtn = false;
+  showNextBtn = true;
 
-  private getUsersPage(startPointDoc = null) {
+  private getUsersPage(startPointDoc = null, nextPage = false, prevPage = false) {
     
     this.dataService.getSystemUsers(startPointDoc).then((usersResponse: any) => {
-      // console.log('usersResponse', usersResponse);
+      console.log('usersResponse', usersResponse);
+      if(this.stillLoading) { this.stillLoading = false; }
+      if(this.stillLoadingUsersPage) { this.stillLoadingUsersPage = false; }
+      if(!usersResponse.length) {
+        this.showNextBtn = false;
+      } else {
+        if(usersResponse.length < 10) {
+          this.showNextBtn = false;
+        } else {
+          this.showNextBtn = true;
+        }
 
-      if(!this.users.length) {
-        this.stillLoading = false;
+        if(nextPage) {
+          this.pagesFirstDocuments.push(usersResponse[0]);
+          this.pagesLastDocuments.push(usersResponse[usersResponse.length - 1]);
+        } else if(prevPage) {
+          this.pagesFirstDocuments.pop();
+        } else {}
+  
+        this.users = usersResponse.map(x => x.data());
+  
+        console.log('this.pagesFirstDocuments', this.pagesFirstDocuments);
+        console.log('this.pagesLastDocuments', this.pagesLastDocuments);
+        if(this.pagesFirstDocuments.length >= 2) {
+          this.showPrevBtn = true;
+        } else {
+          this.showPrevBtn = false;
+        }
       }
-
-      if(true) {
-        this.stillLoading = false;
-      }
-
-      this.usersDocs = usersResponse;
-      this.pagesStartPoints.push(usersResponse[0]);
-      this.users = usersResponse.map(x => x.data());
+ 
     }, (error: any) => {
       console.log('error', error);
     });
   }
 
   ngOnInit() {
-    this.getUsersPage();
+    this.getUsersPage(null, true);
   }
 
   nextPage() {
-    this.stillLoading = true;
-    this.getUsersPage(this.usersDocs[this.usersDocs.length - 1]);
+    this.stillLoadingUsersPage = true;
+    let docRefToStartWith = this.pagesLastDocuments[this.pagesLastDocuments.length - 1]
+    this.getUsersPage(docRefToStartWith, true);
   }
 
   prevPage() {
-    this.stillLoading = true;
-    this.getUsersPage(this.pagesStartPoints[this.pagesStartPoints.length - 2]);
-    this.pagesStartPoints.splice(0, 2);
-    console.log(this.pagesStartPoints)
+    this.stillLoadingUsersPage = true;
+
+    if(this.pagesFirstDocuments.length === 1) {
+      this.getUsersPage();
+    } else {
+      this.pagesFirstDocuments.pop();
+      this.pagesLastDocuments.pop();
+      this.getUsersPage(this.pagesFirstDocuments[this.pagesFirstDocuments.length - 1]);
+    }
   }
 
 }
